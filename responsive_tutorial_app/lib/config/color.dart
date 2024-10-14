e86afdb59dac0282e8eb9d895ad5653ddd8950b9
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-10-14 14:33:40 -0500

    Update project examples

--- responsive_tutorial_app/lib/config/color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primary = Color(0xff1E2875);
-const blueColor = Color(0xff373B5C);
-const blackColor = Color(0xff272830);
-const grayColor = Colors.grey;
-const greenColor = Color(0xff54CC8B);
